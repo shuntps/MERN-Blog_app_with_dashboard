bbad3c3b2806c750b4353adac5a3a726b371e68c
Shunt <shunt_ps@hotmail.com>
2024-06-11 17:41:17 -0400

    Connect to mondodb database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 // Connect to MongoDB and start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
-});
+mongoose
+   .connect(process.env.MONGO_URI)
+   .then(() => {
+      console.log("Connected to MongoDB");
+
+      app.listen(PORT, () => {
+         console.log(`Server running on port ${PORT}`);
+      });
+   })
+   .catch((err) => console.log(err));
