d3c5f067fbf149bb04baa9530c5b084dbe03684f
Shunt <shunt_ps@hotmail.com>
2024-06-13 17:07:58 -0400

    Add middleware function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import bcryptjs from "bcryptjs";
+
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 // Register User
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
    const { username, email, password, confirmPassword } = req.body;
 
    // Trim username and email to remove any leading/trailing spaces
@@ -20,51 +22,47 @@ export const signup = async (req, res) => {
       password === "" ||
       confirmPassword === ""
    ) {
-      return res.status(400).json({ error: "Please fill all required fields" });
+      next(errorHandler(400, "All fields are required"));
    }
 
    // Validate username length and characters
    const usernameRegex = /^[a-zA-Z0-9_]{3,30}$/;
    if (!usernameRegex.test(trimmedUsername)) {
-      return res.status(400).json({
-         error: "Username must be 3-30 characters long and can only contain letters, numbers, and underscores",
-      });
+      next(
+         errorHandler(
+            400,
+            "Username must be 3-30 characters long and can only contain letters, numbers, and underscores"
+         )
+      );
    }
-
-   /*    // Validate password length
-   if (password.length < 6) {
-      return res
-         .status(400)
-         .json({ error: "Password must be at least 6 characters" });
-   } */
 
    // Validate password length and complexity
    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
    if (!passwordRegex.test(password)) {
-      return res.status(400).json({
-         error: "Password must contain at least one uppercase letter, one lowercase letter, and one number, and be at least 6 characters long",
-      });
+      next(
+         errorHandler(
+            400,
+            "Password must contain at least one uppercase letter, one lowercase letter, and one number, and be at least 6 characters long"
+         )
+      );
    }
 
    // Confirm password match
    if (password !== confirmPassword) {
-      return res.status(400).json({ error: "Passwords do not match" });
+      next(errorHandler(400, "Passwords do not match"));
    }
 
+   // Validate email format
    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
    if (!emailRegex.test(trimmedEmail)) {
-      return res
-         .status(400)
-         .json({ error: "Please enter a valid email address" });
+      next(errorHandler(400, "Please enter a valid email address"));
    }
 
    try {
       // Check if the email already exists
       const userExists = await User.findOne({ email: trimmedEmail });
       if (userExists) {
-         return res
-            .status(400)
-            .json({ error: "Email has already been registered" });
+         next(errorHandler(400, "Email has already been registered"));
       }
 
       // Hash the password asynchronously
@@ -81,9 +79,9 @@ export const signup = async (req, res) => {
       await newUser.save();
 
       // Respond with success
-      res.status(201).json({ message: "New user created successfully" });
+      res.status(200).json({ message: "New user created successfully" });
    } catch (error) {
       // Handle errors
-      res.status(500).json({ error: "Something went wrong" });
+      next(error);
    }
 };
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import { errorHandler } from "./utils/error.js";
 
 dotenv.config();
 
@@ -28,3 +29,14 @@ mongoose
 // Routes Middleware
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Error Middleware
+app.use((err, req, res, next) => {
+   const statusCode = err.statusCode || 500;
+   const message = err.message || "Internal Server Error";
+   res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+   });
+});
