7d3707844123441a0ec983f582af0819080abb98
Shunt <shunt_ps@hotmail.com>
2024-08-23 08:25:55 -0400

    Add functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,85 +3,32 @@ import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
-// Register User
 export const signup = async (req, res, next) => {
-   const { username, email, password, confirmPassword } = req.body;
+   const { username, email, password } = req.body;
 
-   // Trim username and email to remove any leading/trailing spaces
-   const trimmedUsername = username.trim();
-   const trimmedEmail = email.trim();
-
-   // Validation
    if (
-      !trimmedUsername ||
-      !trimmedEmail ||
+      !username ||
+      !email ||
       !password ||
-      !confirmPassword ||
-      trimmedUsername === "" ||
-      trimmedEmail === "" ||
-      password === "" ||
-      confirmPassword === ""
+      username === "" ||
+      email === "" ||
+      password === ""
    ) {
       next(errorHandler(400, "All fields are required"));
    }
 
-   // Validate username length and characters
-   const usernameRegex = /^[a-zA-Z0-9_]{3,30}$/;
-   if (!usernameRegex.test(trimmedUsername)) {
-      next(
-         errorHandler(
-            400,
-            "Username must be 3-30 characters long and can only contain letters, numbers, and underscores"
-         )
-      );
-   }
+   const hashedPassword = bcryptjs.hashSync(password, 10);
 
-   // Validate password length and complexity
-   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
-   if (!passwordRegex.test(password)) {
-      next(
-         errorHandler(
-            400,
-            "Password must contain at least one uppercase letter, one lowercase letter, and one number, and be at least 6 characters long"
-         )
-      );
-   }
-
-   // Confirm password match
-   if (password !== confirmPassword) {
-      next(errorHandler(400, "Passwords do not match"));
-   }
-
-   // Validate email format
-   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   if (!emailRegex.test(trimmedEmail)) {
-      next(errorHandler(400, "Please enter a valid email address"));
-   }
+   const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+   });
 
    try {
-      // Check if the email already exists
-      const userExists = await User.findOne({ email: trimmedEmail });
-      if (userExists) {
-         next(errorHandler(400, "Email has already been registered"));
-      }
-
-      // Hash the password asynchronously
-      const hashedPassword = await bcryptjs.hash(password, 10);
-
-      // Create a new user
-      const newUser = new User({
-         username: trimmedUsername,
-         email: trimmedEmail,
-         password: hashedPassword,
-      });
-
-      // Save new user
       await newUser.save();
-
-      // Respond with success
-      res.status(200).json({ message: "New user created successfully" });
+      res.json("Signup successful");
    } catch (error) {
-      // Handle errors
       next(error);
    }
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,50 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+   const navigate = useNavigate();
+   const [formData, setFormData] = useState({});
+   const [errorMessage, setErrorMessage] = useState(null);
+   const [loading, setLoading] = useState(false);
+
+   const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      e.preventDefault();
+      if (!formData.username || !formData.email || !formData.password) {
+         return setErrorMessage("Please fill out all fields.");
+      }
+
+      try {
+         setErrorMessage(null);
+         setLoading(true);
+
+         const res = await fetch("/api/auth/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+         });
+
+         const data = await res.json();
+
+         if (data.success === false) {
+            return setErrorMessage(data.message);
+         }
+         setLoading(false);
+         if (res.ok) {
+            navigate("/sign-in");
+         }
+      } catch (error) {
+         setErrorMessage(error.message);
+         setLoading(false);
+      }
+   };
+
    return (
       <div className="min-h-screen mt-20">
          <div className="flex flex-col p-5 max-w-4xl mx-auto md:flex-row md:items-center gap-10">
@@ -22,18 +65,24 @@ export default function SignUp() {
 
             {/* Right */}
             <div className="flex-1">
-               <form className="flex flex-col gap-4">
+               <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                   <div>
                      <Label value="Your username" />
                      <TextInput
                         type="text"
                         placeholder="Username"
                         id="username"
+                        onChange={handleChange}
                      />
                   </div>
                   <div>
                      <Label value="Your email" />
-                     <TextInput type="email" placeholder="Email" id="email" />
+                     <TextInput
+                        type="email"
+                        placeholder="Email"
+                        id="email"
+                        onChange={handleChange}
+                     />
                   </div>
                   <div>
                      <Label value="Your password" />
@@ -41,21 +90,22 @@ export default function SignUp() {
                         type="password"
                         placeholder="Password"
                         id="password"
-                     />
-                  </div>
-                  <div>
-                     <Label value="Confirm your password" />
-                     <TextInput
-                        type="password"
-                        placeholder="Password"
-                        id="confirmPassword"
+                        onChange={handleChange}
                      />
                   </div>
                   <Button
-                     type="button"
+                     type="submit"
                      className="text-white bg-gradient-to-r from-slate-800 to-slate-500 hover:bg-gradient-to-r hover:from-slate-500 hover:to-slate-800 shadow hover:shadow-lg"
+                     disabled={loading}
                   >
-                     Sign Up
+                     {loading ? (
+                        <>
+                           <Spinner size="sm" />
+                           <span className="pl-3">Loading...</span>
+                        </>
+                     ) : (
+                        "Sign Up"
+                     )}
                   </Button>
                </form>
                <div className="text-center text-sm mt-3">
@@ -67,6 +117,11 @@ export default function SignUp() {
                      Sign In
                   </Link>
                </div>
+               {errorMessage && (
+                  <Alert className="mt-5" color="failure">
+                     {errorMessage}
+                  </Alert>
+               )}
             </div>
          </div>
       </div>
