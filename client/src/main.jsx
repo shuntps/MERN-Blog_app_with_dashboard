6c7b4abc0f43f1f2de7e7e87bd0d65b490902cda
Shunt <shunt_ps@hotmail.com>
2025-01-03 11:16:58 -0500

    Complete profile page UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import './styles/index.css';
+
 import App from './App.jsx';
-import './index.css';
-import { store, persistor } from './redux/store';
 import ThemeProvider from './providers/ThemeProvider.jsx';
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
