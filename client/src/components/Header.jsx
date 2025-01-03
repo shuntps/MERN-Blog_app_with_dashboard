aca7f8387660615f6d22d633ad3d427550e81b14
Shunt <shunt_ps@hotmail.com>
2025-01-03 07:42:16 -0500

    Update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,62 +1,105 @@
-import { Link, useLocation } from "react-router-dom";
-import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { useSelector } from 'react-redux';
+
 import {
-   Button,
-   Navbar,
-   NavbarCollapse,
-   NavbarLink,
-   NavbarToggle,
-   TextInput,
-} from "flowbite-react";
+  Avatar,
+  Button,
+  Dropdown,
+  Navbar,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  TextInput,
+} from 'flowbite-react';
+
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
-   const path = useLocation().pathname;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
-   return (
-      <Navbar className="border-b-2 shadow-sm">
-         <Link
-            to="/"
-            className="self-center whitespace-nowrap font-semibold dark:text-white"
-         >
-            <span className="px-2 py-1 bg-gradient-to-r from-slate-500 to-slate-800 rounded-lg text-white">
-               MyApp
-            </span>
-            Dev
-         </Link>
-         <form>
-            <TextInput
-               type="text"
-               placeholder="Search..."
-               rightIcon={AiOutlineSearch}
-               className="hidden lg:inline"
-            />
-         </form>
-         <Button className="items-center w-12 h-10 lg:hidden" color="gray" pill>
-            <AiOutlineSearch />
-         </Button>
-         <div className="flex gap-2 md:order-2">
-            <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-               <FaMoon />
-            </Button>
-            <Link to="/sign-in">
-               <Button color="gray" outline>
-                  Sign In
-               </Button>
+  return (
+    <Navbar className='border-b-2 shadow-sm'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-slate-500 to-slate-800 rounded-lg text-white'>
+          MyApp
+        </span>
+        Dev
+      </Link>
+
+      <form>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+
+      <Button className='items-center w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt='user'
+                img={`${import.meta.env.VITE_BACKEND_URL}${
+                  currentUser.profilePicture
+                }`}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
-            <NavbarToggle />
-         </div>
-         <NavbarCollapse>
-            <NavbarLink active={path === "/"} as={"div"}>
-               <Link to="/">Home</Link>
-            </NavbarLink>
-            <NavbarLink active={path === "/about"} as={"div"}>
-               <Link to="/about">About</Link>
-            </NavbarLink>
-            <NavbarLink active={path === "/projects"} as={"div"}>
-               <Link to="/projects">Projects</Link>
-            </NavbarLink>
-         </NavbarCollapse>
-      </Navbar>
-   );
+
+            <Dropdown.Divider />
+
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button color='gray' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
+        <NavbarToggle />
+      </div>
+
+      <NavbarCollapse>
+        <NavbarLink active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </NavbarLink>
+        <NavbarLink active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </NavbarLink>
+        <NavbarLink active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
 }
