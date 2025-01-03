02cf7d1bbb207a7fbfc288161625c42223cecfeb
Shunt <shunt_ps@hotmail.com>
2025-01-03 09:58:55 -0500

    Make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Projects from "./pages/Projects";
-import Header from "./components/Header";
-import FooterComp from "./components/Footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import PrivateRoute from './routes/PrivateRoute';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+import FooterComp from './components/Footer';
 
 export default function App() {
-   return (
-      <BrowserRouter>
-         <Header />
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/projects" element={<Projects />} />
-         </Routes>
-         <FooterComp />
-      </BrowserRouter>
-   );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+      </Routes>
+      <FooterComp />
+    </BrowserRouter>
+  );
 }
